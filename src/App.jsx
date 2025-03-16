89d91ba65990b316d2c5e1b357f9eda6ed4cd7ea
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-03-16 20:06:14 +1300

    removed unnecessary file, added Logo-SeamlessStop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchBus from "./pages/SearchBus";
-import SelectStart from "./pages/SelectStart";
-import SelectDestination from "./pages/SelectDestination";
 import ViewMap from "./pages/ViewMap";
 
 //App() component is the root component(App() 컴포넌트는 프로젝트의 최상위 컴포넌트로, 모든 페이지와 UI를 포함한다.)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,10 @@ import "./Header.css";  // Style added
 
 function Header() {
   return (
+    
     <div className="header">
-      <img src="/logo.png" alt="Logo" className="logo" /> 
+      <img src="/logo-AT.png" alt="Logo" className="logo" /> 
+      <img src="/logo-SeamlessStop.png" alt="Second Logo" className="second-logo" /> {/* 새로운 로고 */}
     </div>
   );
 }
